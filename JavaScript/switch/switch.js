b34f9e1589d11a7fa4c130a0280d534dff7625f3
atharva <88647398+atharvahanjankar@users.noreply.github.com>
2022-01-14 22:47:56 -0800

    Add files via upload

---
+++ JavaScript/switch/switch.js
@@ -0,0 +1,33 @@
+// function val(){
+//     var x= document.getElementById('inpAge').valueAsNumber;
+//     switch(true){
+//         case (x >= 12 && x < 18):
+//         case (x > 18 && x < 25):
+//             document.getElementById('ans').value="age is less than 50";
+//             break;
+//         default:
+//             document.getElementById('ans').value="Enter a valid value";
+//     }
+// }
+
+            // document.getElementById('ans').value="Age is less than 50";
+            // break;
+
+function loop(){
+    var x = 10;
+    // for(var i=0;i<x;i++)
+    // {
+    //     console.log(i);
+    //     document.getElementById('prt').innerHTML=i;
+    // }
+    let i=0;
+    // while(i < x){
+    //     console.log(i);
+    //     i++;
+    // }
+    do{
+        console.log(i);
+        i++
+    }
+    while(i<x);
+}
